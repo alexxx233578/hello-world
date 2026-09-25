71e0def19ec240e5437fa80abeb282a18bf1ecaa
alexx233578 <alybelly1202@.com>
2026-09-25 01:12:06 +0000

    Change output to Hello World!

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
-// This program prints Hello, world!
+// This program prints Hello World!
 #include <stdio.h>
 
 int main() {
-  printf("Hello, world!\n");
+  printf("Hello World!\n");
 return 0;
 }
